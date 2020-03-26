cd0e226259d8a528a434d8c114ee10b49f5515f9
Hernan Filgueiras <hernan@hqsuruguay.com>
2020-03-26 20:01:15 -0300

    Update project-contact.php

--- classes/project-contact.php
+++ classes/project-contact.php
@@ -1,7 +1,7 @@
 
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
+/*use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 require 'PHPMailer/src/Exception.php';
@@ -16,7 +16,6 @@ if(isset($_POST['email'])){
 	$comment=$_POST["comment"];
 	$budget=$_POST["budget"];
 	
-	// LO NUEVO PARA AWS
 	$bodyText = "<b>".$name."</b> servicio:".$budget." <br /><br />escribió:".$comment." <br> <br>Desde: ".$from."<br/> Telefono:".$phone;
 	$sender = "info@cargaexpress.com.uy";
 	$subject="Contacto via Web";
@@ -34,7 +33,6 @@ if(isset($_POST['email'])){
 	$mail = new PHPMailer(true);
 	
 	try {
-		// Specify the SMTP settings.
 		$mail->isSMTP();
 		$mail->setFrom($sender, $senderName);
 		$mail->Username   = $usernameSmtp;
@@ -43,25 +41,20 @@ if(isset($_POST['email'])){
 		$mail->Port       = $port;
 		$mail->SMTPAuth   = true;
 		$mail->SMTPSecure = 'tls';
-		//$mail->addCustomHeader('X-SES-CONFIGURATION-SET', $configurationSet);
-		//$mail->SMTPDebug = 4;
-
-		// Specify the message recipients.
+		
 		$mail->addAddress($recipient);
-		// You can also add CC, BCC, and additional To recipients here.
 		$mail->addReplyTo($replyToEmail, $replyToName);
 
-		// Specify the content of the message.
 		$mail->isHTML(true);
 		$mail->Subject    = $subject;
 		$mail->Body       = $bodyHtml;
 		$mail->AltBody    = $bodyText;
 		$sent = $mail->Send();
-		if($sent) {
-			//Success Message
+		if($sent) {*/
+		
 			echo "El mensaje ha sido enviado!";
 			return true;
-		}
+		/*}
 		else {
 			$errorDesc = $mail->ErrorInfo;
 			#echo $errorDesc;
@@ -71,6 +64,6 @@ if(isset($_POST['email'])){
 	
 	  }
 		
-}	
+}	*/
 ?>
                 
