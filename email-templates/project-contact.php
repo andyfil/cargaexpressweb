7eedcba31ba55859ed4ce5612432171009977e93
Hernan Filgueiras <hernan@hqsuruguay.com>
2020-03-26 19:09:14 -0300

    contact form actualizado

--- email-templates/project-contact.php
+++ email-templates/project-contact.php
@@ -21,30 +21,15 @@ if(isset($_POST['email'])){
 	$sender = "info@cargaexpress.com.uy";
 	$subject="Contacto via Web";
 
-	// php mailer last version
-	// Replace sender@example.com with your "From" address.
-	// This address must be verified with Amazon SES.
 	$senderName = $name;
-	// Replace recipient@example.com with a "To" address. If your account
-	// is still in the sandbox, this address must be verified.
 	$recipient = 'info@cargaexpress.com.uy';
 	$replyToName = $name;
 	$replyToEmail = $from; 
-	// Replace smtp_username with your Amazon SES SMTP user name.
 	$usernameSmtp = 'AKIA5DC55FYN4CSLNN6G';
-	// Replace smtp_password with your Amazon SES SMTP password.
 	$passwordSmtp = 'BCKLn065TCA6CjmEi77OCho2uWwH9TGDxAWNl/BMNhGJ';
-	// Specify a configuration set. If you do not want to use a configuration
-	// set, comment or remove the next line.
-	//$configurationSet = 'ConfigSet';
-
-	// If you're using Amazon SES in a region other than US West (Oregon),
-	// replace email-smtp.us-west-2.amazonaws.com with the Amazon SES SMTP
-	// endpoint in the appropriate region.
 	$host = 'email-smtp.us-east-1.amazonaws.com';
 	$port = 587;
-	// The HTML-formatted body of the email
-	$bodyHtml = str_replace('\n',"<br />",$mensaje);
+	$bodyHtml = str_replace('\n',"<br />",$bodyText);
 
 	$mail = new PHPMailer(true);
 	
